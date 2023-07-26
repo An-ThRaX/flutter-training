aa3b06c910adc3d8c6b36f8fc201aa91c6c096b2
An-ThRaX <ciprianionutz93@gmail.com>
2023-07-26 15:03:59 +0200

    new commit

--- handle_a_list2.dart
+++ handle_a_list2.dart
@@ -1,28 +1,50 @@
 // an approach where the user defines the list at the terminal
 import 'dart:io';
 void main(){
-  // variables to be used
-  var list = [];
-  var newList =[];
-  int listLength, max_value = 0;
+  // // variables to be used
+  // var list = [];
+  // var newList =[];
+  // int listLength, max_value = 0;
 
-  print("How many elements should the list have? ");
-  listLength = int.parse(stdin.readLineSync()!);  // determine the list length
+  // print("How many elements should the list have? ");
+  // listLength = int.parse(stdin.readLineSync()!);  // determine the list length
   
-  print("Please insert the values now.");  // populate the list
-  while (list.length < listLength){
-    var input = int.parse(stdin.readLineSync()!);  
-    list.add(input);
-  }
+  // print("Please insert the values now.");  // populate the list
+  // while (list.length < listLength){
+  //   var input = int.parse(stdin.readLineSync()!);  
+  //   list.add(input);
+  // }
   
-  for (var i = 0; i < list.length; i++){  // iterate the list
-    if(list[i].isEven){
-      newList.add(list[i]);  // if the value is even, add it to a special list
+  // for (var i = 0; i < list.length; i++){  // iterate the list
+  //   if(list[i].isEven){
+  //     newList.add(list[i]);  // if the value is even, add it to a special list
+  //   }
+  //   if (list[i] > max_value){
+  //     max_value = list[i];  // find the highest value in the list
+  //   } 
+  // }
+  // print("All the even numbers are: $newList.");
+  // print("The highest number in the list is: $max_value.");
+
+
+  // define a method to do the task
+  List<int> listHandler(){
+    List<int> input_list = [];
+    print("Input the list elements. When done, type 'exit' to stop!");
+
+    while(true){
+      String input = stdin.readLineSync()!;
+      if( input.toLowerCase() == 'exit'){
+        break;
+      }
+
+      input_list.add(int.parse(input));
     }
-    if (list[i] > max_value){
-      max_value = list[i];  // find the highest value in the list
-    } 
+    return(input_list);
   }
-  print("All the even numbers are: $newList.");
-  print("The highest number in the list is: $max_value.");
+
+// call the methods to get the result
+  List<int> test_list = listHandler();
+  print(test_list.runtimeType);
+
 }
